8b3c948b5bfde8be3d1e6511f7d48e1b016768c0
karim1556 <karimshaikh1555@gmail.com>
2024-07-01 16:00:49 +0530

    fixed typos

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         </span>
       </h1>
       <p className="desc text-center">
-      IdeaScape is a cutting-edge, open-source AI tool for today's world, offering the ability to explore, create, and circulate innovative prompts." 
+      IdeaScape is a cutting-edge, open-source AI tool for today's world, offering the ability to explore, create, and circulate innovative prompts.
       </p>
       <Feed/>
     </section>
